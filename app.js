d4aadc051587ad8b7d9736ed43a7785d9863e521
leodini <leodini@hotmail.com>
2020-07-15 17:53:54 -0300

    animations

--- app.js
+++ app.js
@@ -8,3 +8,34 @@ let body = selectElement('body')
 menuToggler.addEventListener('click', () => {
 	body.classList.toggle('open')
 })
+
+// scroll reveal
+window.sr = ScrollReveal()
+
+sr.reveal('.animate-left', {
+	origin: 'left',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300,
+})
+
+sr.reveal('.animate-right', {
+	origin: 'right',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300,
+})
+
+sr.reveal('.animate-top', {
+	origin: 'top',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300,
+})
+
+sr.reveal('.animate-bottom', {
+	origin: 'bottom',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300,
+})
